3bcbe676d64ffb164b6e7b61d439173e850db37c
Moses Mwongela <moses1889@gmail.com>
2017-08-09 14:24:45 +0000

    just incase

--- src/Movies.js
+++ src/Movies.js
@@ -6,8 +6,62 @@ import {
 } from 'react-native';
 import { movies } from './data';
 import MoviePoster from './MoviePoster';
+import MoviePopup from './MoviePopup';
 
 export default class Movies extends Component {
+
+  state = {
+    popupIsOpen: false,
+    // Day chosen by user
+    chosenDay: 0,       // choose first day by default
+    // Time chosen by user
+    chosenTime: null,
+  }
+
+  openMovie = (movie) => {
+    this.setState({
+      popupIsOpen: true,
+      movie,  
+    });
+  }
+
+  closeMovie = () => {
+    this.setState({
+      popupIsOpen: false,
+      // Reset values to default ones
+      chosenDay: 0,
+      chosenTime: null,
+    });
+  }
+
+  chooseDay = (day) => {
+    this.setState({
+      chosenDay: day,
+    });
+  }
+
+  chooseTime = (time) => {
+    this.setState({
+      chosenTime: time,
+    });
+  }
+
+  bookTicket = () => {
+    // Make sure they selected time 
+    if (!this.state.chosenTime) {
+      alert('Please select show time');
+    } else {
+      // Close popup
+      this.closeMovie();
+      // Navigate away to Confirmation route
+      this.props.navigator.push({
+        name: 'confirmation',
+        // Generate random string
+        code: Math.random().toString(36).substring(6).toUpperCase(),
+      });
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -22,6 +76,18 @@ export default class Movies extends Component {
                           key={index} 
             />)}
         </ScrollView>
+
+        <MoviePopup
+          movie={this.state.movie}
+          isOpen={this.state.popupIsOpen}
+          onClose={this.closeMovie}
+          chosenDay={this.state.chosenDay}
+          chosenTime={this.state.chosenTime}
+          onChooseDay={this.chooseDay}
+          onChooseTime={this.chooseTime}
+          onBook={this.bookTicket}
+        />
+
       </View>
     );
   }
--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import CustomComponents from 'react-native-deprecated-custom-components';
 import Movies from './Movies';
+import Confirmation from './Confirmation';
 
 const RouteMapper = (route, navigator) => {
   if (route.name === 'movies') {
-    return <Movies navigator={navigator} />;
+    return (
+      <Movies navigator={navigator} />
+    );
+  } else if (route.name === 'confirmation') {
+    return (
+      <Confirmation code={route.code} navigator={navigator} />
+    );
   }
 };
 
